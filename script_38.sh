034ce70db65db4b11c829913268d7bbd21fbff4d
pigr3co <pigrecoinfinito@gmail.com>
2019-06-23 10:12:11 +0200

    corretta variabile

--- script_38.sh
+++ script_38.sh
@@ -1,22 +1,8 @@
-## descrizione script
-
-Il file eseguibile (per esempio:`QGIS-OSGeo4W-3.4.9-1-Setup-x86_64.exe`) è un file zippato, per unzipparlo in ambiente linux occorre installare un tool, io ho scelto [questo](https://manpages.debian.org/jessie/p7zip-full/7za.1.en.html):
-
-per installarlo in linux:
-
-`sudo apt-get install p7zip-full`
-
----
-
-**script:**
-
-
-```bash
 #!/bin/bash
 
 set -x
 # variabile con nome versione
-versione=QGIS-OSGeo4W-3.4.9-1-Setup-x86_64
+versione=QGIS-OSGeo4W-3.8.0-1-Setup-x86_64
 # scarico il file eseguibile
 curl http://download.osgeo.org/qgis/win64/"${versione}".exe >"${versione}".exe
 # unzippo il file eseguibile
@@ -28,13 +14,13 @@ mv '$PLUGINSDIR' OSGeo4W
 #cambio cartella
 cd OSGeo4W/bin
 # rinomino file caso LTR
-mv qgis-ltr.bat.tmpl qgis-ltr.bat
+# mv qgis-ltr.bat.tmpl qgis-ltr.bat
 # rinomino file caso non LTR
-# mv qgis.bat.tmpl qgis.bat
+mv qgis.bat.tmpl qgis.bat
 # cambio cartella caso LTR
-cd ../apps/qgis-ltr/bin
+# cd ../apps/qgis-ltr/bin
 # cambio cartella caso non LTR
-# cd ../apps/qgis/bin
+cd ../apps/qgis/bin
 # scarico cartella zippata
 curl -L "https://github.com/pigreco/QGIS_portable_3x/raw/master/file/win64/win64.zip" >win64.zip
 # unzippo cartella
@@ -44,5 +30,4 @@ rm win64.zip
 # cambio cartella
 cd ../../../../
 # creo unica cartella zippata, adeguare i numeri finali
-7z a OSGeo4W_349.7z OSGeo4W
-```
+7z a OSGeo4W_380.7z OSGeo4W
